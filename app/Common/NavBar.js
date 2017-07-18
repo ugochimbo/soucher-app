4055a6f9272f2ea4d0668bbb64269e643124e503
Ugo Ejikeme <me@ugochimbo.com>
2017-07-18 19:18:36 +0200

    updated nav bar

--- app/Common/NavBar.js
+++ app/Common/NavBar.js
@@ -8,11 +8,21 @@ export default class NavBar extends Component{
             <header id="header">
                 <h1><Link to="/">SOU<span style={{"color": "#6a707c"}}>CHER </span></Link></h1>
                 <nav>
-                    <Link to={LINK_TO.GIFT_SOUCHER_ROUTE} className="button">Gift A Soucher</Link>
-                    <Link to={LINK_TO.SWAP_SOUCHER_ROUTE} className="button">Swap A Soucher</Link>
-                    <Link to={LINK_TO.GIFT_CARD_CATALOG_ROUTE} className="button">Voucher Catalog</Link>
-                    <Link to={LINK_TO.ABOUT_US_ROUTE} className="button">About Us</Link>
-                    <Link to={LINK_TO.CONTACT_US_ROUTE} className="button">Contact Us</Link>
+                    <Link to={LINK_TO.GIFT_SOUCHER_ROUTE} className="button">
+                        <span className="icon fa-gift" /> Gift
+                    </Link>
+                    <Link to={LINK_TO.GIFT_CARD_CATALOG_ROUTE} className="button">
+                        <span className="icon fa-book" /> Catalog
+                    </Link>
+                    <Link to={LINK_TO.SWAP_SOUCHER_ROUTE} className="button">
+                        <span className="icon fa-keyboard-o" /> Swap
+                    </Link>
+                    <Link to={LINK_TO.ABOUT_US_ROUTE} className="button">
+                        <span className="icon fa-group" /> About
+                    </Link>
+                    <Link to={LINK_TO.CONTACT_US_ROUTE} className="button">
+                        <span className="icon fa-phone-square" /> Contact
+                    </Link>
                 </nav>
             </header>
         )
