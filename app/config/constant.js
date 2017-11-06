08d053763354fbacdc06d3d9757e9cfdb35d47f9
Ugo Ejikeme <me@ugochimbo.com>
2017-11-06 01:37:17 +0100

    hotfix react nested route bug

--- app/config/constant.js
+++ app/config/constant.js
@@ -2,16 +2,15 @@
 export const HOME = '/';
 
 export const GIFT_SOUCHER_ROUTE = '/gift';
-export const GIFT_SOUCHER_SUCCESS_ROUTE = '/gift/success';
+export const GIFT_SOUCHER_SUCCESS_ROUTE = '/gift-success';
 
 export const SWAP_SOUCHER_ROUTE = '/swap';
-export const SWAP_SOUCHER_SUCCESS_ROUTE = '/swap/success';
-export const SWAP_SOUCHER_CANCEL_ROUTE = '/swap/cancel';
+export const SWAP_SOUCHER_SUCCESS_ROUTE = '/swap-success';
+export const SWAP_SOUCHER_CANCEL_ROUTE = '/swap-cancelled';
 
 export const GIFT_CARD_CATALOG_ROUTE = '/catalog';
-export const GIFT_CARD_CATALOG_FILTER_ROUTE = '/catalog/filter';
 
 export const ABOUT_US_ROUTE = '/about-us';
 export const CONTACT_US_ROUTE = '/contact-us';
 
-export const TRANSACTION_ERROR_ROUTE = '/transaction/error';
+export const TRANSACTION_ERROR_ROUTE = '/transaction-error';
