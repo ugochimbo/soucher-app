84a5646c8cfb582436c4eb8b5996199a0510c011
Ugo Ejikeme <me@ugochimbo.com>
2018-01-08 08:39:07 +0100

    implemented mobile nav bar

--- app/Common/NavBar.js
+++ app/Common/NavBar.js
@@ -6,8 +6,8 @@ export default class NavBar extends Component{
       render() {
         return (
             <header id="header">
-                <h1><Link to="/">SOU<span style={{"color": "#6a707c"}}>CHER </span></Link></h1>
-                <nav>
+                <h1><Link to="/">SOU<span id="cher">CHER </span></Link></h1>
+                <nav className='desktop'>
                     <Link to={LINK_TO.GIFT_SOUCHER_ROUTE} className="button">
                         <span className="icon fa-gift" /> Gift
                     </Link>
@@ -23,6 +23,48 @@ export default class NavBar extends Component{
                     <Link to={LINK_TO.CONTACT_US_ROUTE} className="button">
                         <span className="icon fa-phone-square" /> Contact Us
                     </Link>
+                </nav>
+
+                <nav className='mobile'>
+                    <a href="#menu"><i className="fa fa-bars" aria-hidden="true">
+                        <span style={{'marginLeft': '2px'}}>Menu</span> </i></a>
+                </nav>
+
+                <nav id="menu">
+                    <div className="inner">
+                    <h2>
+                        <i className="fa fa-bars" aria-hidden="true" />
+                        Menu
+                        <i className="fa fa-bars" aria-hidden="true" />
+                    </h2>
+                    <ul className="links">
+                        <li>
+                            <Link to={LINK_TO.GIFT_SOUCHER_ROUTE}>
+                                <span className="icon fa-gift" /> Gift Soucher
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={LINK_TO.GIFT_CARD_CATALOG_ROUTE}>
+                                <span className="icon fa-book" /> View Catalog
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={LINK_TO.SWAP_SOUCHER_ROUTE}>
+                                <span className="icon fa-money" /> Redeem Soucher
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={LINK_TO.ABOUT_US_ROUTE}>
+                                <span className="icon fa-group" /> About Us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={LINK_TO.CONTACT_US_ROUTE}>
+                                <span className="icon fa-phone-square" /> Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                    </div>
                 </nav>
             </header>
         )
